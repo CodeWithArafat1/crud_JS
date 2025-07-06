efaa817ec329a8b9757621eef34beb0ee757740a
CodeWithArafat1 <arafatnill2.0@gmail.com>
2025-07-06 12:07:03 +0600

    no reload commit

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const addBtn = document.getElementById("add");
 const createForm = document.getElementById("createForm");
 const updateForm = document.getElementById("updateForm");
+const addBtn = document.getElementById("add");
+
 const tBody = document.getElementById("tBody");
 
+const newId = document.getElementById("newId");
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 
@@ -12,32 +14,7 @@ function add() {
 }
 
 window.onload = () => {
-  const saved = getData();
-
-  saved.forEach((users) => {
-    const tr = document.createElement("tr");
-
-    const tdName = document.createElement("td");
-    tdName.innerText = users.id;
-    tr.appendChild(tdName);
-
-    const tdId = document.createElement("td");
-    tdId.innerText = users.name;
-    tr.appendChild(tdId);
-
-    const tdEmail = document.createElement("td");
-    tdEmail.innerText = users.email;
-    tr.appendChild(tdEmail);
-
-    const actionBtn = document.createElement("td");
-    actionBtn.innerHTML = `<div class="action-buttons">
-    <button class="edit-btn" onclick="edit(${users.id})">Edit</button>
-    <button class="delete-btn" onclick="delUsers(${users.id})">Delete</button> 
-    </div>`;
-    tr.appendChild(actionBtn);
-
-    tBody.appendChild(tr);
-  });
+  renderTable();
 };
 
 function addUsers() {
@@ -46,91 +23,124 @@ function addUsers() {
 
   const name = nameInput.value.trim();
   const email = emailInput.value.trim();
-  if (name === "" || email === "") {
-    return;
-  }
+  const id = Date.now();
+  newId.value = id;
 
-  const newId = Date.now();
   const tr = document.createElement("tr");
 
   const tdId = document.createElement("td");
-  const tdName = document.createElement("td");
-  const tdEmail = document.createElement("td");
-  const actionBtn = document.createElement("td");
-
-  tdId.innerText = newId;
-  tdName.innerText = name;
-  tdEmail.innerText = email;
-
-  actionBtn.innerHTML = `<div class="action-buttons">
-    <button class="edit-btn" onclick="edit(${newId})">Edit</button>
-    <button class="delete-btn" onclick="delUsers(${newId})">Delete</button> 
-    </div>`;
-
+  tdId.innerText = id;
   tr.appendChild(tdId);
+
+  const tdName = document.createElement("td");
+  tdName.innerText = name;
   tr.appendChild(tdName);
+
+  const tdEmail = document.createElement("td");
+  tdEmail.innerText = email;
   tr.appendChild(tdEmail);
+
+  const actionBtn = document.createElement("td");
+  actionBtn.innerHTML = `
+  <div class="action-buttons">
+              <button class="edit-btn" onclick="edit(${id})">Edit</button>
+              <button class="delete-btn" onclick="delUser(${id})">delete</button>
+</div>`;
   tr.appendChild(actionBtn);
 
   tBody.appendChild(tr);
-  const oldData = getData();
 
-  const newUsers = {
-    id: newId,
-    name: name,
-    email: email,
-  };
-  oldData.push(newUsers);
+  const getFromLocal = getData();
+  getFromLocal.push({ id: id, name, email });
+  setData(getFromLocal);
 
-  setData(oldData);
   nameInput.value = "";
   emailInput.value = "";
 }
 
 function getData() {
-  return JSON.parse(localStorage.getItem("allUsers")) || [];
+  return JSON.parse(localStorage.getItem("userData")) || [];
 }
-function setData(oldData) {
-  return localStorage.setItem("allUsers", JSON.stringify(oldData));
+
+function setData(data) {
+  return localStorage.setItem("userData", JSON.stringify(data));
 }
 
 function edit(id) {
-  updateForm.style.display = "flex";
-  addBtn.style.display = "none";
-  createForm.style.display = "none";
+  const getUsers = getData();
 
-  const users = getData();
+  const findUsers = getUsers.find((item) => item.id === id);
 
-  const findUsers = users.find((item) => item.id === id);
+  const newId = document.getElementById("newId");
+  const uname = document.getElementById("uname");
+  const uemail = document.getElementById("uemail");
+
+  newId.value = findUsers.id;
   uname.value = findUsers.name;
   uemail.value = findUsers.email;
-  document.getElementById("newId").value = findUsers.id;
-}
 
-function delUsers(id) {
-  const users = getData();
-  const delUser = users.filter((item) => item.id !== id);
-
-  setData(delUser);
-  location.reload();
+  updateForm.style.display = "flex";
+  addBtn.style.display = "none";
 }
 
 function updateUser() {
-  updateForm.style.display = "none";
-  addBtn.style.display = "flex";
-  const users = getData();
+  const getUsers = getData();
   const uid = parseInt(document.getElementById("newId").value);
-  const unameValue = document.getElementById("uname").value.trim();
-  const uemailValue = document.getElementById("uemail").value.trim();
+  const uname = document.getElementById("uname").value.trim();
+  const uemail = document.getElementById("uemail").value.trim();
 
-  const updateUsers = users.map((item) => {
-    if (item.id === uid) {
-      return { ...item, name: unameValue, email: uemailValue };
+  const filterUsers = getUsers.map((data) => {
+    if (data.id === uid) {
+      return { ...data, name: uname, email: uemail };
     } else {
-      return item;
+      return data;
     }
   });
 
-  setData(updateUsers);
-  location.reload();
+  setData(filterUsers);
+  renderTable();
+
+  updateForm.style.display = "none";
+  addBtn.style.display = "block";
+}
+
+function renderTable() {
+  const getUsers = getData();
+  tBody.innerHTML = "";
+
+  getUsers.forEach((data) => {
+    const tr = document.createElement("tr");
+
+    const tdId = document.createElement("td");
+    tdId.innerText = data.id;
+    tr.appendChild(tdId);
+
+    const tdName = document.createElement("td");
+    tdName.innerText = data.name;
+    tr.appendChild(tdName);
+
+    const tdEmail = document.createElement("td");
+    tdEmail.innerText = data.email;
+    tr.appendChild(tdEmail);
+
+    const actionBtn = document.createElement("td");
+    actionBtn.innerHTML = `
+  <div class="action-buttons">
+              <button class="edit-btn" onclick="edit(${data.id})">Edit</button>
+              <button class="delete-btn" onclick="delUser(${data.id})">delete</button>
+</div>`;
+    tr.appendChild(actionBtn);
+
+    tBody.appendChild(tr);
+  });
+}
+
+function delUser(id) {
+  const getUsers = getData();
+
+  const filteredUsers = getUsers.filter((data) => data.id !== id);
+
+  setData(filteredUsers);
+
+  renderTable();
 }
